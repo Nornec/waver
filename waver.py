f8e0b19b55c997352d0c32612c3a7e4091f55dc7
dreamer <dreamer@puikheid.nl>
2024-10-30 17:55:49 +0100

    make it more pythonic

--- waver.py
+++ waver.py
@@ -3,7 +3,7 @@
 #``````````````
 # waver
 # Turns a mono .wav file (or the left channel of a stereo .wav file) into raw data and chucks the percentage value of the data into a .wtd file. The .wtd file format is used specifically for Midinous to read in as a wavetable, but it is stored as encoded plain text
-# 
+#
 # .wtd format:
 #  line | description
 #     1 | wavetable name (alphanumeric only, starting with an alpha character or underscore)
@@ -21,7 +21,7 @@
 # INITIALIZATION |
 #`````````````````
 
-import sys, os, os.path, math, warnings
+import sys, warnings
 from scipy.io import wavfile
 from pathlib import Path
 
@@ -29,15 +29,16 @@ from pathlib import Path
 # DEFINITIONS |
 #``````````````
 
-data = [];
 MAX_INT = 2147483647
+
 
 #_________
 # UTILITY |
 #``````````
 
-#Function to return the percent value of an integer value
-def get_pct(num):
+def get_pct(num: int) -> float:
+    """ Function to return the percent value of an integer value
+    """
     if num > 0:
         return num/MAX_INT
     elif num < 0:
@@ -45,23 +46,27 @@ def get_pct(num):
     else:
         return 0
 
+
 #________
 # TASKS  |
 #`````````
 
-#Get the raw data values from a wave file using scipy
-def get_data_from_wav(f):
-    global data
-    #Sometimes, a .wav will have header information that can't be converted or read from in this way. Suppress those warnings
+def get_data_from_wav(f: Path, data: list) -> list:
+    """ Get the raw data values from a wave file using scipy
+    """
+    # Sometimes, a .wav will have header information that can't be converted or read from in this way. Suppress those warnings
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         _, data = wavfile.read(f)
 
-#Write data values to the output file, formatted for 8 decimal places, then close the file
-def write_output(filename, wavename):
-    global data
+    return data
+
+
+def write_output(filename: str, wavename: str, data: list):
+    """ Write data values to the output file, formatted for 8 decimal places, then close the file
+    """
     out_file = open(filename, "w")
-    out_file.write(wavename + "\n")
+    out_file.write(f"{wavename}\n")
     line_count = 0
     for v in data:
         if line_count == len(data) - 1:
@@ -69,37 +74,42 @@ def write_output(filename, wavename):
             out_file.close()
             return
         else:
-            out_file.write(f'{get_pct(v):.8f}' + "\n")
+            out_file.write(f"{get_pct(v):.8f}\n")
         line_count += 1
     out_file.close()
 
-#Checks to see if the input waveform is a .wav, then proceeds with other functions
+
 def execute():
+    """ Checks to see if the input waveform is a .wav, then proceeds with other functions
+    """
+    data = []
+
     input_filename_short = input("Input wave file name (short):")
-    input_filename = "./input/" + input_filename_short + ".wav"
-        
+    input_filename = f"./input/{input_filename_short}.wav"
+
     input_path = Path(input_filename)
-    if input_path.is_file() == False:
+    if not input_path.is_file():
         print("Input file doesn't exist or format is not a .wav")
         sys.exit()
     else:
-        get_data_from_wav(input_filename)
+        data = get_data_from_wav(input_path, data)
 
     wavetable_name = input("Wavetable name:")
     while wavetable_name.strip()[0].isdigit():
         print("First character of wavetable name should be a letter...\n")
         wavetable_name = input("Wavetable name:")
     wavetable_name = wavetable_name.strip()
-    wavetable_name = wavetable_name.replace(" ","_")
+    wavetable_name = wavetable_name.replace(" ", "_")
 
-    output_filename = "./output/" + input_filename_short + ".wtd"
+    output_filename = f"./output/{input_filename_short}.wtd"
 
-    write_output(output_filename, wavetable_name)
-    print("Done! Find your file in the output directory: " + output_filename)
+    write_output(output_filename, wavetable_name, data)
+    print(f"Done! Find your file in the output directory: {output_filename}")
+
 
 #_______
 # MAIN |
 #```````
 
-execute()
-exit()
+if __name__ == "__main__":
+    execute()
